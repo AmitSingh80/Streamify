cb0373e34c9e619a70cedf822b6e3c0a85d7ef42
AmitSingh80 <heyamitsingh7@gmail.com>
2025-06-20 03:13:45 +0530

    streamify first push backend ,frontend code

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export  async function signup (req,res){
+    res.send("signup routes");
+}
+
+export async function login (req,res){
+    res.send("Login routes");
+}
+
+export async function logout (req,res){
+    res.send("Logut routes");
+}
---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+
+
+
+
+
+
+
+dotenv.config()
+
+const app= express();
+const PORT=process.env.PORT
+
+
+
+// app.get("/ping",(req,res)=>{
+//     res.send("pong")
+// })
+
+app.use("/api/auth",authRoutes)
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port: ${PORT}`);
+    connectDB()
+    
+})
