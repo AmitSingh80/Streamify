a57914c2da374a718ad0e3c3c3562cb1b148cb83
AmitSingh80 <heyamitsingh7@gmail.com>
2025-07-03 18:13:41 +0530

    call , video , chat page done

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -4,6 +4,7 @@ import useAuthUser from "../hooks/useAuthUser";
 import { useQuery } from "@tanstack/react-query";
 import { getStreamToken } from "../lib/api";
 
+
 import {
   Channel,
   ChannelHeader,
@@ -20,24 +21,25 @@ import toast from "react-hot-toast";
 import ChatLoader from "../components/ChatLoader";
 import CallButton from "../components/CallButton";
 
-const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY
+// console.log("Stream api key" ,STREAM_API_KEY);
 
 const ChatPage = () => {
-  const { id: targetUserId } = useParams();
+  const {id:targetUserId} =useParams();
 
-  const [chatClient, setChatClient] = useState(null);
+   const [chatClient, setChatClient] = useState(null);
   const [channel, setChannel] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const { authUser } = useAuthUser();
+  const {authUser} = useAuthUser();
 
-  const { data: tokenData } = useQuery({
+  const {data:tokenData} = useQuery({
     queryKey: ["streamToken"],
     queryFn: getStreamToken,
-    enabled: !!authUser, // run only when authUser is available
-  });
-
-  useEffect(() => {
+    enabled: !!authUser   //run only when user available
+  })
+  
+      useEffect(() => {
     const initChat = async () => {
       if (!tokenData?.token || !authUser) return;
 
@@ -77,38 +79,44 @@ const ChatPage = () => {
     };
 
     initChat();
-  }, [tokenData, authUser, targetUserId]);
+   }, [tokenData, authUser, targetUserId]);
 
-  const handleVideoCall = () => {
-    if (channel) {
-      const callUrl = `${window.location.origin}/call/${channel.id}`;
+    const handleVideoCall =()=> {
+         if(channel){
+          const callUrl = `${window.location.origin}/call/${channel.id}`;
 
       channel.sendMessage({
-        text: `I started a video call. Plz Join me here: ${callUrl}`,
+        text: `I start  a video call. plz Join me here: ${callUrl}`,
       });
 
       toast.success("Video call link sent successfully!");
     }
-  };
 
-  if (loading || !chatClient || !channel) return <ChatLoader/>;
+         }
+    
+
+
+
+     if (loading || !chatClient || !channel) return <ChatLoader/>;
+
 
   return (
     <div className="h-[93vh]">
-      <Chat client={chatClient}>
+      <Chat client = {chatClient}>
         <Channel channel={channel}>
           <div className="w-full relative">
-            <CallButton handleVideoCall={handleVideoCall} />
+            <CallButton handleVideoCall = {handleVideoCall}/>
             <Window>
-              <ChannelHeader />
-              <MessageList />
-              <MessageInput focus />
+              <ChannelHeader/>
+              <MessageList/>
+              <MessageInput focus/>
             </Window>
           </div>
-          <Thread />
+          <Thread/>
         </Channel>
       </Chat>
     </div>
-  );
-};
-export default ChatPage;
+  )
+}
+
+export default ChatPage
